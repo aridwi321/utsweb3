7c002ea553795096fa1f09410f512e30c0449b0d
aridwi321 <aridwiprasetia12@gmail.com>
2019-04-11 08:53:11 +0700

    Updates

---
+++ precache-manifest.80037390cc938f1909f8e17c40a8383a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "371dd3676839bd68e7ae",
+    "url": "/utsweb3/static/js/runtime~main.371dd367.js"
+  },
+  {
+    "revision": "ae315fdf15d5dcb7355f",
+    "url": "/utsweb3/static/js/main.ae315fdf.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/utsweb3/static/js/2.9eee67cf.chunk.js"
+  },
+  {
+    "revision": "ae315fdf15d5dcb7355f",
+    "url": "/utsweb3/static/css/main.96d3d868.chunk.css"
+  },
+  {
+    "revision": "7eca390267639476bac621e4b21e3131",
+    "url": "/utsweb3/index.html"
+  }
+];
